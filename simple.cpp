12abff57add1e3d2b3c4889cd4634d587974062a
Brandon Wong <bwong7@hawaii.edu>
2019-04-23 14:00:44 -1000

    Added simple program

---
+++ simple.cpp
@@ -0,0 +1,17 @@
+/* 
+	File: simple.c
+	Created by Brandon Wong on 4/23/19
+*/
+
+
+#include <iostream>
+
+
+int main(int argc, char *argv[])
+{
+	std::cout << "This is a simple program" << std::endl;
+
+	// Add changes anywhere below this line
+
+}
+
